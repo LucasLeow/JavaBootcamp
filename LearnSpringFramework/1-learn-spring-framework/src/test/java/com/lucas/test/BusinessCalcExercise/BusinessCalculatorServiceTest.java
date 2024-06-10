d428289dc5c9b6df6fdb66d5b314eb6ed3207d30
LucasLeow <leowjianhoong@gmail.com>
2024-06-10 08:44:01 +0800

    Init spring-jpa-hibernate learning project

---
+++ LearnSpringFramework/1-learn-spring-framework/src/test/java/com/lucas/test/BusinessCalcExercise/BusinessCalculatorServiceTest.java
@@ -0,0 +1,12 @@
+package com.lucas.test.BusinessCalcExercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessCalculatorServiceTest {
+
+    @Test
+    void findMax() {
+    }
+}
---
+++ learn-spring-jpa-hibernate/src/test/java/com/lucas/springboot/learn_spring_jpa_hibernate/LearnSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.springboot.learn_spring_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
