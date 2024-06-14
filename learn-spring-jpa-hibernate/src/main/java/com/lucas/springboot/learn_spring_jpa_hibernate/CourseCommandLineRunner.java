ba7d263e5983241cfd6229c3cffe72f65a0b2c5d
Lucas Leow <leowjianhoong@gmail.com>
2024-06-14 20:12:28 +0800

    Implement Insert & Delete by Id using JDBC

---
+++ learn-spring-jpa-hibernate/src/main/java/com/lucas/springboot/learn_spring_jpa_hibernate/CourseCommandLineRunner.java
@@ -0,0 +1,20 @@
+package com.lucas.springboot.learn_spring_jpa_hibernate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJdbcRepository repo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repo.insert(new Course(1, "Learning Java", "Lucas"));
+        repo.insert(new Course(2, "Learning JavaScript", "Lucas"));
+        repo.deleteById(2);
+
+    }
+}
